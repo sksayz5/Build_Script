e9d6c6a6602852f9250fe60d60f9082ec0febcef
sksayz5 <137992110+sksayz5@users.noreply.github.com>
2024-09-11 13:00:28 +0530

    Update ci_bot.sh

--- ci_bot.sh
+++ ci_bot.sh
@@ -2,14 +2,14 @@
 mkdir -p ~/work
 cd ~/work
 
-git clone https://github.com/Shra1V32/cpu-usage-aggregator.git
+git clone https://github.com/sksayz5/cpu-usage-aggregator.git
 cd cpu-usage-aggregator
 
 sudo pip install -r requirements.txt
 
 sudo apt-get install acct
 
-sudo python main.py && cd ..
+sudo python main.py && cd ../..
 
 git config --global user.name "Shravan55555"
 git config --global user.email "srasidda.ks@gmail.com"
